4825dc81abf3c96766ab1e06343e0e33e9e7fb5e
Marco Marinho <marcomarinho12@gmail.com>
2017-04-03 22:58:22 +0100

    Revert "Telephony: Enable LTE for GSM LTE phones (part I)"
    This reverts commit 023bc5c144a5c222df68465745790529c6716a1e.
    
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -3389,8 +3389,4 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
             phone.setPolicyDataEnabled(enabled);
         }
     }
-
-    public int getLteOnGsmMode() {
-        return mPhone.getLteOnGsmMode();
-    }
 }
